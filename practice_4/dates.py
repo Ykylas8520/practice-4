d4b509a6d1b6f4d0c83cd4028fdb498cb12716f2
Ykylas8520 <ykylasnurmukhammed@gmail.com>
2026-03-06 10:11:29 +0500

    Update dates.py

--- practice_4/dates.py
+++ practice_4/dates.py
@@ -1,22 +1,22 @@
-from datetime import datetime, timedelta
+import datetime
 
-# Example 1: Current date and time
+# Example 1: 
 a = datetime.now()
 print(a)
 
-# Example 2: Create specific date
+# Example 2: 
 b = datetime(2024, 5, 10)
 print(b)
 
-# Example 3: Date formatting
+# Example 3: 
 print(a.strftime("%Y-%m-%d"))
 print(a.strftime("%H:%M:%S"))
 
-# Example 4: Time difference
+# Example 4: 
 d1 = datetime(2024, 1, 1)
 d2 = datetime(2024, 1, 10)
 print(d2 - d1)
 
-# Example 5: Add days
+# Example 5: 
 new_date = d1 + timedelta(days=7)
 print(new_date)
